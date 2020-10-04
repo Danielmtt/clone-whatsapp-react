172bbbd1c45a1b0d681b574a0f8ec41c12f4a5cb
Daniel Mota <daniel.mota@iteris.com.br>
2020-10-04 20:29:05 -0300

    Updates

---
+++ precache-manifest.6c1ac129b0dc0a9ccd9343aa29d42305.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37323f6b8969c5cc54de741769cf18ab",
+    "url": "/clone-whatsapp-react/index.html"
+  },
+  {
+    "revision": "39e8fa0557f347322169",
+    "url": "/clone-whatsapp-react/static/css/main.bb923463.chunk.css"
+  },
+  {
+    "revision": "b264c8f5a94e21a82065",
+    "url": "/clone-whatsapp-react/static/js/2.86b3435c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clone-whatsapp-react/static/js/2.86b3435c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e8fa0557f347322169",
+    "url": "/clone-whatsapp-react/static/js/main.af2f9164.chunk.js"
+  },
+  {
+    "revision": "84b6ba36c1cd47cf2a7f",
+    "url": "/clone-whatsapp-react/static/js/runtime-main.14fcd270.js"
+  },
+  {
+    "revision": "bfca0bd1bc14972cd346615cc8b08db7",
+    "url": "/clone-whatsapp-react/static/media/nenhumContatoSelecionado.bfca0bd1.png"
+  },
+  {
+    "revision": "49ce7e49355c1135c0e1993360dd6b17",
+    "url": "/clone-whatsapp-react/static/media/wallpaper_areamensagem.49ce7e49.png"
+  }
+]);
